62fc1227c518fa62e67dcfdf0880d5265fd2df71
Mrwan Zein <mrwanzein@outlook.com>
2018-07-01 14:24:15 -0400

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
