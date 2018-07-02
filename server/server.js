5d0b67f70033e66ad2776f346bfcbeabef5b5dce
Mrwan Zein <mrwanzein@outlook.com>
2018-07-02 13:25:27 -0400

    added GET /todos/id:

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+let {ObjectID} = require('mongodb');
 
 let app = express();
 
@@ -29,6 +30,25 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+     return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+         return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(3000, () => {
