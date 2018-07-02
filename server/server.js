9a46072c8d0c61ff878908ac161eadc167ac4064
Mrwan Zein <mrwanzein@outlook.com>
2018-07-02 14:24:04 -0400

    Setuo app for heroku deployment

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let {User} = require('./models/user');
 let {ObjectID} = require('mongodb');
 
 let app = express();
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -51,8 +52,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
 
 
