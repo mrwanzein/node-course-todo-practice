e1bb9e0c06d2e8f47f83129559d31d12779bda19
Mrwan Zein <mrwanzein@outlook.com>
2018-07-03 12:24:10 -0400

    Added todos delete route in server

--- server/server.js
+++ server/server.js
@@ -52,6 +52,24 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid Id');
+       }
+
+    Todo.findByIdAndRemove(id).then((doc) => {
+        if (!doc) {
+            return res.status(404).send('Did not found doc');
+        }
+        
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send();
+    });   
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
