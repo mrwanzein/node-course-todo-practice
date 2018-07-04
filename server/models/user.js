3126107b8ea076a57a66d907061135be648f00ff
Mrwan Zein <mrwanzein@outlook.com>
2018-07-04 14:56:53 -0400

    Added auth methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,59 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-let User = mongoose.model('User', {
-    user: {
-     type: String,
-         required: true,
-         trim: true,
-         minLenght: 1
-     }, 
-     email: {
-         type: String,
-         required: true,
-         trim: true,
-         minLenght: 1
-     }
- });
+let UserSchema = new mongoose.Schema({
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            minLenght: 1,
+            unique: true, // when using this validator, make sure to restart the appropriate database
+            validate: {
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email'
+            }
+           },
+            password: {
+                type: String,
+                required: true,
+                minLenght: 6
+            },
+            tokens: [{
+                access: {
+                    type: String,
+                    required: true
+                },
+                token: {
+                   type: String,
+                   required: true
+                }
+            }]     
+   
+});
+
+UserSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']); // what the user can see
+  
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this; // does not work in arrow function
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access}, 'youandme').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+let User = mongoose.model('User', UserSchema);
 
  module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -24,6 +24,19 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']); // choose what data the user can temper with
+    let user = new User(body);
+
+    user.save().then(() => {
+       return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({
