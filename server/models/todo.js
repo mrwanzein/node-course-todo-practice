498f4ae67783e571e560f23da8b873a6aa2fcf43
Mrwan Zein <mrwanzein@outlook.com>
2018-07-09 15:37:24 -0400

    Associate users with todos

--- server/models/todo.js
+++ server/models/todo.js
@@ -14,6 +14,10 @@ let Todo = mongoose.model('Todo' /*collection name*/, {
     completedAt: {
         type: String,
         default: null
+    },
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -13,9 +13,10 @@ const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     let todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -23,6 +24,18 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
+        res.send({
+            todos
+        });
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
 app.post('/users', (req, res) => {
@@ -60,16 +73,6 @@ app.delete('/users/me/token', authenticate, (req, res) => {
     }, () => {
         res.status(400).send();
     })
-});
-
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({
-            todos
-        });
-    }, (err) => {
-        res.status(400).send(err);
-    });
 });
 
 app.get('/todos/:id', (req, res) => {
